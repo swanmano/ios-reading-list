acbfd7ae72c90ca82388e7f939e17d938a03b1c9
Craig Swanson <cswanson02@me.com>
2019-10-03 11:53:31 -0500

    Update through step 8 except for delete function. Still need to do that.

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -9,8 +9,21 @@
 import Foundation
 
 class BookController {
+    
+    // MARK: Properties
     var books: [Book] = []
     
+    var readBook: [Book] {
+        books.filter({$0.hasBeenRead == true})
+    }
+    
+    var unreadBook: [Book] {
+        books.filter({$0.hasBeenRead == false})
+    }
+    
+    // TODO: possibly write an init for savetopersistentstore() here?
+    
+    // MARK: Saving and loading to/from property list
     private var readingListURL: URL? {
         let filemanager = FileManager.default
         guard let documents = filemanager.urls(for: .documentDirectory, in: .userDomainMask).first
@@ -44,5 +57,31 @@ class BookController {
         } catch {
             print("Error loading books data: \(error)")
         }
+    }
+    
+    // MARK: Methods
+    func create(bookTitle title: String, reasonToRead reason: String) -> Book {
+        let book = Book(title: title, reasonToRead: reason)
+        books.append(book)
+        saveToPersistentStore()
+        return book
+    }
+    
+    func delete(_ book: Book) {
+        if let index = books.index(of: book) {
+            books.remove(at: index)
+        }
+        saveToPersistentStore()
+    }
+    
+    func updateHasBeenRead(for book: Book) -> Book {
+        var book = book
+        book.hasBeenRead.toggle()
+        return book
+    }
+    
+    // TODO: create a working editBook function
+    func editBook(for book: Book) -> Book {
+        return book
     }
 }
